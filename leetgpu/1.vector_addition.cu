f4799ae2dc260f00acc12d5177000363b2bf718b
nnarek <n.nazaretyan.5@gmail.com>
2025-05-24 08:17:53 +0000

    new solutions and script to download submissions from leetgpu

---
+++ leetgpu/1.vector_addition.cu
@@ -0,0 +1,21 @@
+// Gpu: NVIDIA TESLA T4
+// Runtime: 1.15245 ms
+#include "solve.h"
+#include <cuda_runtime.h>
+
+__global__ void vector_add(const float* A, const float* B, float* C, int N) {
+    int globalId = (blockIdx.x * blockDim.x) + threadIdx.x;
+    if(globalId < N) {
+        C[globalId] = A[globalId] + B[globalId];
+    }
+}
+
+// A, B, C are device pointers (i.e. pointers to memory on the GPU)
+void solve(const float* A, const float* B, float* C, int N) {
+    int threadsPerBlock = 256;
+    int blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
+
+    vector_add<<<blocksPerGrid, threadsPerBlock>>>(A, B, C, N);
+    cudaDeviceSynchronize();
+}
+
---
+++ leetgpu/7.color_inversion.cu
@@ -0,0 +1,22 @@
+// Gpu: NVIDIA TESLA T4
+// Runtime: 0.76354 ms
+#include "solve.h"
+#include <cuda_runtime.h>
+
+__global__ void invert_kernel(unsigned char* image, int width, int height) {
+    int pixel_index = (blockIdx.x * blockDim.x) + threadIdx.x;
+    if(pixel_index < width * height)
+    {
+        image[4*pixel_index] = 255 - image[4*pixel_index];
+        image[4*pixel_index+1] = 255 - image[4*pixel_index+1];
+        image[4*pixel_index+2] = 255 - image[4*pixel_index+2];
+    }
+}
+// image_input, image_output are device pointers (i.e. pointers to memory on the GPU)
+void solve(unsigned char* image, int width, int height) {
+    int threadsPerBlock = 256;
+    int blocksPerGrid = (width * height + threadsPerBlock - 1) / threadsPerBlock;
+
+    invert_kernel<<<blocksPerGrid, threadsPerBlock>>>(image, width, height);
+    cudaDeviceSynchronize();
+}
