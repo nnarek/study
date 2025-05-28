b6ad0de878b791270d8df3882037cd45ad733652
nnarek <n.nazaretyan.5@gmail.com>
2025-05-28 18:31:11 +0000

    improved solution by using atomicAdd instead of block level reduction

--- leetgpu/4.reduction.cu
+++ leetgpu/4.reduction.cu
@@ -1,6 +1,6 @@
 // URL: https://leetgpu.com/challenges/reduction
 // GPU: NVIDIA TESLA T4
-// Runtime: 2.39442 ms
+// Runtime: 2.28479 ms
 #include "solve.h"
 #include <cuda_runtime.h>
 #include <iostream>
@@ -23,50 +23,13 @@ __global__ void sum(const float* input, float* output, int N) {
         __syncthreads();
     }
     if(threadIdx.x == 0) {
-        output[blockIdx.x] = sharedMem[0];
+        atomicAdd(output,sharedMem[0]);
     }
 }
 
 // input, output are device pointers
 void solve(const float* input, float* output, int N) {  
-    if(N == 1) {
-        cudaMemcpy(output, input, sizeof(float),cudaMemcpyDeviceToDevice);
-        return;
-    } else if(N <= threadsPerBlock) {
-        sum<<<1, threadsPerBlock>>>(input, output, N);
-        cudaDeviceSynchronize();
-        return;
-    }
-    float *temp_data = NULL;
-
-    cudaError_t err;
-
-    const float* current_buffer = input;
-    int length = N;
-    
-    int blocksPerGrid = (length + threadsPerBlock - 1) / threadsPerBlock;
-    size_t memSize = blocksPerGrid * sizeof(float);
-    err = cudaMalloc((void**)&temp_data, 2*memSize);
-    if (err != cudaSuccess) {
-        std::cerr << "CUDA error: " << cudaGetErrorString(err) << std::endl;
-        exit(4);
-    }
-    float *temp_data1 = temp_data;
-    float *temp_data2 = temp_data+blocksPerGrid;
-    while (1 < length) {
-        int blocksPerGrid = (length + threadsPerBlock - 1) / threadsPerBlock;
-        sum<<<blocksPerGrid, threadsPerBlock>>>(current_buffer, temp_data1, length);
-        cudaDeviceSynchronize();
-        current_buffer = temp_data1;
-        std::swap(temp_data1,temp_data2);        
-        length = blocksPerGrid;
-    }
-
-    cudaMemcpy(output, current_buffer, sizeof(float),cudaMemcpyDeviceToDevice);
-
-    err = cudaFree(temp_data);
-    if (err != cudaSuccess) {
-        std::cerr << "CUDA error: " << cudaGetErrorString(err) << std::endl;
-        exit(5);
-    }   
+    int blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
+    sum<<<blocksPerGrid, threadsPerBlock>>>(input, output, N);
+    cudaDeviceSynchronize();  
 }
