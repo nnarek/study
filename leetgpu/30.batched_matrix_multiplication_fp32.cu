cafefbfb6b372dc52c195fe5d770655d01f29e55
nnarek <n.nazaretyan.5@gmail.com>
2025-06-13 11:03:29 +0000

    improved performance(little bit) by using streams to run kernels concurrently

--- leetgpu/30.batched_matrix_multiplication_fp32.cu
+++ leetgpu/30.batched_matrix_multiplication_fp32.cu
@@ -1,6 +1,6 @@
 // URL: https://leetgpu.com/challenges/batched-matrix-multiplication-fp32
 // GPU: NVIDIA TESLA T4
-// Runtime: 2.75808 ms
+// Runtime: 2.74412 ms
 #include "solve.h"
 #include <cuda_runtime.h>
 
@@ -18,20 +18,36 @@ __global__ void matrix_multiplication_kernel(const float* A, const float* B, flo
 }
 
 // A, B, C are device pointers (i.e. pointers to memory on the GPU)
-void matrix_multiplication(const float* A, const float* B, float* C, int M, int N, int K) {
+void matrix_multiplication(const float* A, const float* B, float* C, int M, int N, int K, cudaStream_t stream) {
     dim3 threadsPerBlock(16, 16);
     dim3 blocksPerGrid((K + threadsPerBlock.x - 1) / threadsPerBlock.x,
                        (M + threadsPerBlock.y - 1) / threadsPerBlock.y);
     
-    matrix_multiplication_kernel<<<blocksPerGrid, threadsPerBlock>>>(A, B, C, M, N, K);
+    matrix_multiplication_kernel<<<blocksPerGrid, threadsPerBlock, 0, stream>>>(A, B, C, M, N, K);
 }
 
 
 // A, B, C are device pointers
 void solve(const float* A, const float* B, float* C, int BATCH, int M, int N, int K) {
-    for (int batch_index = 0; batch_index < BATCH; ++batch_index) {
-        matrix_multiplication(A+M*K*batch_index, B+K*N*batch_index, C+M*N*batch_index, M, K, N);
+
+    const int max_num_streams = 32;
+    cudaStream_t streams[max_num_streams];
+    const int num_streams = min(max_num_streams,BATCH);
+    
+    for (int i = 0; i < num_streams; ++i) {
+        cudaStreamCreate(&streams[i]);
     }
-    cudaDeviceSynchronize();
+
+    for (int batch_index = 0; batch_index < BATCH; ++batch_index) {
+        const float* A_ptr = A + M * K * batch_index;
+        const float* B_ptr = B + K * N * batch_index;
+        float* C_ptr = C + M * N * batch_index;
+        matrix_multiplication(A_ptr, B_ptr, C_ptr, M, K, N, streams[batch_index%num_streams]);
+    }
+    cudaDeviceSynchronize();//waiting to all streams
+    
+    for (int i = 0; i < num_streams; ++i) {
+        cudaStreamDestroy(streams[i]);
+    }
 }
     
