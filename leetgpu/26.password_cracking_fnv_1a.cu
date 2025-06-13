e65dd3cc8270b5b3da943181d9bcb80c8711b2c8
nnarek <n.nazaretyan.5@gmail.com>
2025-06-13 19:10:06 +0000

    improved by generating code for each possible length of password

--- leetgpu/26.password_cracking_fnv_1a.cu
+++ leetgpu/26.password_cracking_fnv_1a.cu
@@ -1,40 +1,42 @@
 // URL: https://leetgpu.com/challenges/password-cracking-fnv-1a
 // GPU: NVIDIA TESLA T4
-// Runtime: 0.00565 ms
+// Runtime: 0.00412 ms
 #include "solve.h"
 #include <cuda_runtime.h>
 
 // if we know that 1<=password_length<=6 then possible combinations are 25^6=244140625
 
 
-const int maxPasswordLength = 6;
-
 // FNV-1a hash function that takes a byte array and its length as input
 // Returns a 32-bit unsigned integer hash value
-__device__ unsigned int fnv1a_hash_bytes(const unsigned char* data, int length) {
+template<int length>
+__device__ unsigned int fnv1a_hash_bytes(const unsigned char* data) {
     const unsigned int FNV_PRIME = 16777619;
     const unsigned int OFFSET_BASIS = 2166136261;
     
     unsigned int hash = OFFSET_BASIS;
+    #pragma unroll 
     for (int i = 0; i < length; i++) {
         hash = (hash ^ data[i]) * FNV_PRIME;
     }
     return hash;
 }
 
-__global__ void fnv1a_hash_kernel(unsigned int target_hash, int password_length, int R, char* output_password,int numCombinations) {
+template<int password_length>
+__global__ void fnv1a_hash_kernel(unsigned int target_hash, int R, char* output_password,int numCombinations) {
 
     int idx = (blockIdx.x * blockDim.x) + threadIdx.x;
     if(idx < numCombinations) {
-        unsigned char password[maxPasswordLength];
+        unsigned char password[password_length];
+        #pragma unroll 
         for(int it=0; it < password_length; ++it) {
             password[it] = 'a' + (idx%26);
             idx = idx/26;
         }
 
-        unsigned int hash_int = fnv1a_hash_bytes(password,password_length);
+        unsigned int hash_int = fnv1a_hash_bytes<password_length>(password);
         for(int i = 1; i < R; ++i) {
-            hash_int = fnv1a_hash_bytes((const unsigned char*)&hash_int,4);
+            hash_int = fnv1a_hash_bytes<4>((const unsigned char*)&hash_int);
         }
 
         if(hash_int == target_hash) {//threads of other blocks will continue execution 
@@ -46,9 +48,21 @@ __global__ void fnv1a_hash_kernel(unsigned int target_hash, int password_length,
 
 
 // output_password is a device pointer
-void solve(unsigned int target_hash, int password_length, int R, char* output_password) {
+template<int password_length>
+void solve_for_given_pass_length(unsigned int target_hash, int R, char* output_password) {
     int numCombinations = pow(26,password_length);
     int threadsPerBlock = 256;
     int blocksPerGrid = (numCombinations + threadsPerBlock - 1)/threadsPerBlock;
-    fnv1a_hash_kernel<<<blocksPerGrid,threadsPerBlock>>>(target_hash, password_length, R, output_password,numCombinations);
+    fnv1a_hash_kernel<password_length><<<blocksPerGrid,threadsPerBlock>>>(target_hash, R, output_password,numCombinations);
+}
+// assuming that password_length can not exceed 6
+void solve(unsigned int target_hash, int password_length, int R, char* output_password) {
+    switch(password_length) {
+        case 1: solve_for_given_pass_length<1>(target_hash,R,output_password); return;
+        case 2: solve_for_given_pass_length<2>(target_hash,R,output_password); return;
+        case 3: solve_for_given_pass_length<3>(target_hash,R,output_password); return;
+        case 4: solve_for_given_pass_length<4>(target_hash,R,output_password); return;
+        case 5: solve_for_given_pass_length<5>(target_hash,R,output_password); return;
+        case 6: solve_for_given_pass_length<6>(target_hash,R,output_password); return;
+    }
 }
