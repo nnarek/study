4e36bc98703e39af96591c8685242a4329a72a38
nnarek <n.nazaretyan.5@gmail.com>
2025-06-17 13:05:12 +0000

    improved little bit by using less number of threads per block and also using strided loop to copy input into shared memory

--- leetgpu/10.2d_convolution.cu
+++ leetgpu/10.2d_convolution.cu
@@ -1,6 +1,6 @@
 // URL: https://leetgpu.com/challenges/2d-convolution
 // GPU: NVIDIA TESLA T4
-// Runtime: 6.67979 ms
+// Runtime: 6.0791 ms
 #include "solve.h"
 #include <cuda_runtime.h>
 
@@ -8,33 +8,33 @@
 __global__ void convolution_2d_kernel(const float* input, const float* kernel, float* output,
                                       int input_rows, int input_cols, int kernel_rows, int kernel_cols) {
     __shared__ float shared_kernel[1024];//more than 31*31
-    __shared__ float shared_input[4096];//(blockDim.x+kernel_rows)*(blockDim.x+kernel_cols) < 64*64
+    __shared__ float shared_input[2304];
 
     const int output_rows = input_rows - kernel_rows + 1;
     const int output_cols = input_cols - kernel_cols + 1;
     const int idx = blockIdx.x * blockDim.x + threadIdx.x;
     const int idy = blockIdx.y * blockDim.y + threadIdx.y;
-    const int shared_input_rows = blockDim.x + kernel_rows;//current block can only reach to max(threadIdx.x+kidx)_rd row, which is blockDim.x+kernel_rows
-    const int shared_input_cols = blockDim.y + kernel_cols;
+    const int shared_input_rows = blockDim.x + kernel_rows - 1;//current block can only reach to max(threadIdx.x+kidx)_rd row, which is blockDim.x+kernel_rows
+    const int shared_input_cols = blockDim.y + kernel_cols - 1;
+    const int input_length = input_rows*input_cols;
 
     if(threadIdx.x < kernel_rows && threadIdx.y < kernel_cols) {
         shared_kernel[threadIdx.x*kernel_cols + threadIdx.y] = kernel[threadIdx.x*kernel_cols + threadIdx.y];
     }
-    if (idx < input_rows && idy < input_cols) {
-        shared_input[threadIdx.x*shared_input_cols+threadIdx.y] = input[idx*input_cols+idy];
-    }
-    if((blockDim.x+idx) < input_rows && (blockDim.y+idy) < input_cols 
-    && threadIdx.x < kernel_rows && threadIdx.y < kernel_cols) {
-        shared_input[(blockDim.x+threadIdx.x)*shared_input_cols+blockDim.y+threadIdx.y] = input[(blockDim.x+idx)*input_cols+blockDim.y+idy];
-    }
-    // in first try I forgot to copy this right and bottom side rectangles and only copyied right-bottom small rectangle and main square
-    if((blockDim.x+idx) < input_rows && idy < input_cols 
-    && threadIdx.x < kernel_rows) {
-        shared_input[(blockDim.x+threadIdx.x)*shared_input_cols+threadIdx.y] = input[(blockDim.x+idx)*input_cols+idy];
-    }
-    if(idx < input_rows && (blockDim.y+idy) < input_cols 
-    && threadIdx.y < kernel_cols) {
-        shared_input[threadIdx.x*shared_input_cols+blockDim.y+threadIdx.y] = input[idx*input_cols+blockDim.y+idy];
+    const int num_threads = blockDim.x * blockDim.y;// num of threads available in this block
+    const int shared_input_length = shared_input_rows*shared_input_cols; // num of shared inputs which should be copied
+    const int num_iter = (shared_input_length + num_threads - 1) / num_threads;
+    for(int i=0; i < num_iter; ++i) {
+        const int shared_tid = num_threads*i + threadIdx.x*blockDim.y + threadIdx.y;
+        const int shared_tidx = shared_tid / shared_input_cols;
+        const int shared_tidy = shared_tid - shared_tidx*shared_input_cols;
+        const int idx = shared_tidx + blockIdx.x*blockDim.x;
+        const int idy = shared_tidy + blockIdx.y*blockDim.y; 
+        const int input_id = idx*input_cols+idy;
+        if(input_id < input_length) {
+            shared_input[shared_tid] = input[input_id];
+        }
+        //memory is sufficient because num_iter=((31+16-1)*(31+16-1)+16*16-1)/16/16=9 in worst case and we need 9*16*16 memory
     }
 
     __syncthreads();
@@ -53,7 +53,7 @@ __global__ void convolution_2d_kernel(const float* input, const float* kernel, f
 // input, kernel, output are device pointers
 void solve(const float* input, const float* kernel, float* output,
            int input_rows, int input_cols, int kernel_rows, int kernel_cols) {
-    dim3 threadsPerBlock(32, 32);
+    dim3 threadsPerBlock(16, 16);
     const int output_rows = input_rows - kernel_rows + 1;
     const int output_cols = input_cols - kernel_cols + 1;
     dim3 blocksPerGrid((output_rows + threadsPerBlock.x - 1) / threadsPerBlock.x,
