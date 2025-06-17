ab43300496febbed4fce93118aed599e29ed9782
nnarek <n.nazaretyan.5@gmail.com>
2025-06-17 12:57:11 +0000

    fixed absence of cudaDeviceSynchronize and also exiting newly started threads if password already found

--- leetgpu/26.password_cracking_fnv_1a.cu
+++ leetgpu/26.password_cracking_fnv_1a.cu
@@ -1,6 +1,6 @@
 // URL: https://leetgpu.com/challenges/password-cracking-fnv-1a
 // GPU: NVIDIA TESLA T4
-// Runtime: 0.00412 ms
+// Runtime: 0.00411 ms
 #include "solve.h"
 #include <cuda_runtime.h>
 
@@ -23,8 +23,10 @@ __device__ unsigned int fnv1a_hash_bytes(const unsigned char* data) {
 }
 
 template<int password_length>
-__global__ void fnv1a_hash_kernel(unsigned int target_hash, int R, char* output_password,int numCombinations) {
-
+__global__ void fnv1a_hash_kernel(unsigned int target_hash, int R, char* output_password,int numCombinations, int* found_flag) {
+    if(*found_flag == 1) {//no need to do synchronized read because false positive results are not dangerous
+        return;
+    }
     int idx = (blockIdx.x * blockDim.x) + threadIdx.x;
     if(idx < numCombinations) {
         unsigned char password[password_length];
@@ -42,6 +44,7 @@ __global__ void fnv1a_hash_kernel(unsigned int target_hash, int R, char* output_
         if(hash_int == target_hash) {//threads of other blocks will continue execution 
             memcpy(output_password,password,password_length);
             output_password[password_length] = 0;
+            *found_flag = 1;
         }
     }
 }
@@ -50,10 +53,15 @@ __global__ void fnv1a_hash_kernel(unsigned int target_hash, int R, char* output_
 // output_password is a device pointer
 template<int password_length>
 void solve_for_given_pass_length(unsigned int target_hash, int R, char* output_password) {
+    int *found_flag;
+    cudaMallocAsync((void **)&found_flag, sizeof(int), 0);
+    cudaMemsetAsync(found_flag, 0, sizeof(int),0);
+
     int numCombinations = pow(26,password_length);
     int threadsPerBlock = 256;
     int blocksPerGrid = (numCombinations + threadsPerBlock - 1)/threadsPerBlock;
-    fnv1a_hash_kernel<password_length><<<blocksPerGrid,threadsPerBlock>>>(target_hash, R, output_password,numCombinations);
+    fnv1a_hash_kernel<password_length><<<blocksPerGrid,threadsPerBlock>>>(target_hash, R, output_password,numCombinations,found_flag);
+    cudaDeviceSynchronize();  
 }
 // assuming that password_length can not exceed 6
 void solve(unsigned int target_hash, int password_length, int R, char* output_password) {
