375b431523506260ca4d1797cb867e15387a624e
nnarek <n.nazaretyan.5@gmail.com>
2025-06-19 16:55:28 +0000

    improved very little by using simd instruction

--- leetgpu/7.color_inversion.cu
+++ leetgpu/7.color_inversion.cu
@@ -1,6 +1,6 @@
 // URL: https://leetgpu.com/challenges/color-inversion
 // GPU: NVIDIA TESLA T4
-// Runtime: 0.68709 ms
+// Runtime: 0.68684 ms
 #include "solve.h"
 #include <cuda_runtime.h>
 
@@ -8,10 +8,8 @@ __global__ void invert_kernel(unsigned char* image, int width, int height) {
     int pixel_index = (blockIdx.x * blockDim.x) + threadIdx.x;
     if(pixel_index < width * height)
     {
-        unsigned int* point = (unsigned int*)(image+4*pixel_index);
-        const auto point_masked = *point | 0x00FFFFFF; //preserving last char and asigning 255 to others 
-        const auto point_without_last = *point & 0x00FFFFFF; //assigning 0 to last char and preserving others
-        *point = point_masked - point_without_last; //will not cause overflow for non of chars because of constraints of this challenge
+        unsigned int* point = (unsigned int*)(image+4*pixel_index);//assuming that image is 4 byte aligned
+        *point = __vabsdiffu4(0x00FFFFFF,*point); //this simd instruction calculates abs difference for each byte
     }
 }
 // image_input, image_output are device pointers (i.e. pointers to memory on the GPU)
