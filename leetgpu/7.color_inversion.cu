45bdb756e50b01b08ca8842d53960cad92dc71b6
nnarek <n.nazaretyan.5@gmail.com>
2025-06-19 16:53:05 +0000

    improved by calculating results of 3 uchars at once

--- leetgpu/7.color_inversion.cu
+++ leetgpu/7.color_inversion.cu
@@ -1,6 +1,6 @@
 // URL: https://leetgpu.com/challenges/color-inversion
 // GPU: NVIDIA TESLA T4
-// Runtime: 0.76354 ms
+// Runtime: 0.68709 ms
 #include "solve.h"
 #include <cuda_runtime.h>
 
@@ -8,9 +8,10 @@ __global__ void invert_kernel(unsigned char* image, int width, int height) {
     int pixel_index = (blockIdx.x * blockDim.x) + threadIdx.x;
     if(pixel_index < width * height)
     {
-        image[4*pixel_index] = 255 - image[4*pixel_index];
-        image[4*pixel_index+1] = 255 - image[4*pixel_index+1];
-        image[4*pixel_index+2] = 255 - image[4*pixel_index+2];
+        unsigned int* point = (unsigned int*)(image+4*pixel_index);
+        const auto point_masked = *point | 0x00FFFFFF; //preserving last char and asigning 255 to others 
+        const auto point_without_last = *point & 0x00FFFFFF; //assigning 0 to last char and preserving others
+        *point = point_masked - point_without_last; //will not cause overflow for non of chars because of constraints of this challenge
     }
 }
 // image_input, image_output are device pointers (i.e. pointers to memory on the GPU)
