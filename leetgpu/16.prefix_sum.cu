d5559619a1df08295b0fb7c2490fa57c46cf22f7
nnarek <n.nazaretyan.5@gmail.com>
2025-07-05 10:59:40 +0000

    optimized using warp level operations

--- leetgpu/16.prefix_sum.cu
+++ leetgpu/16.prefix_sum.cu
@@ -1,61 +1,71 @@
 // URL: https://leetgpu.com/challenges/prefix-sum
 // GPU: NVIDIA TESLA T4
-// Runtime: 0.13132 ms
+// Runtime: 0.12229 ms
 #include "solve.h"
 #include <cuda_runtime.h>
+#include <cooperative_groups.h>
 
-// first i have solved problem by dynamic programming
-// we can use half of threads to solve problem for first half of array and also use other threads for second half. 
-// after that we need to add last element of first half to all elements of second half
-// now we need to implement this using loops, also we need to do almost same with results of each block
-// 0->1 2->3 4->5 
-// sharedMem[2*threadIdx.x+1] += sharedMem[2*threadIdx.x];
+// to perform computation by using warp level sata transfer mechanisms, each thread 
+// always need to change its dedicated memory and can not read variables of other threads 
+// and atomically write into variable of another thread
+// if in some point of time, we have buckets with size bucketDim 
+// then threads of every second bucket should add last elem of previous bucket
+// bucketId of given thread is threadIdx.x/bucketDim, last elem of previous bucket is bucketId*bucketDim-1
+// thread should add this elem only if bucketId%2==1
+// we can use masks to include only threads of every second bucket
+// or we can try to make so that calulation of last elem of previous bucket will return out of bound value for previous buckets of every second bucket
+// at the begining mask should be 0xAAAAAAAA (same as ... 1010 1010)  
+// in second iteration it should be 0xCCCCCCCC (same as ... 1100 1100)
+// for out of bound threads, shfl will return val of current thread, so we need to multiply it by 0 in this case, and there is no need for masks 
 
-//   0    1   2    3   ... threads ids
-// 1->2 1->3 5->6 5->7   ... should be added from->to by corresponding thread
-// sharedMem[2*(1+threadIdx.x/2)+threadIdx.x] += sharedMem[4*(threadIdx.x/2)+1];
 
-// in general after n iteration we will have solved subarray with size 2^n(note that last subarray can have less size than 2^n)
-// and each time we need to add last element of first subarray to all elements of second subarrat, and do same for 3rd,5th ... subarrays
-// hence tid-rd thread should add to all elements of 2*(tid/2^n)+1 subarray, which first element is located at 2^n*(2*(tid/2^n)+1)
-// and each of that threads should add value from element with index 2^n*(2*(tid/2^n)+1)-1
-// local index of given thread within its group is tid%2^n
-// so we need to do folowing on each iteration
-// shMem[2^n*(2*(tid/2^n)+1) + tid%2^n] += shMem[2^n*(2*(tid/2^n)+1)-1]
-// which can be simplified to 
-// shMem[2^n*(tid/2^n)+2^n + tid] += shMem[2^n*(2*(tid/2^n)+1)-1]
+constexpr int threadsPerBlock = 512;
 
-// in the level of block we have same problem but in this case group size is bigger or equal to threadsPerBlock, so we can try to reuse code
-
-const int threadsPerBlock = 1024;
+namespace cg = cooperative_groups;
 
 __global__ void prefix_sum(const float* input, float* output, int N) {
-    __shared__ float shMem[threadsPerBlock];
+    constexpr int warpSize = 32;
+    constexpr int numWarpsPerBlock = threadsPerBlock / warpSize;
 
-    int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    if(idx >= N) {
-        return;
+    __shared__ float warp_last[numWarpsPerBlock];// last elements of outputs of each warp
+
+    const auto idx = blockIdx.x * threadsPerBlock + threadIdx.x;
+
+    cg::thread_block block = cg::this_thread_block();
+    cg::thread_block_tile<warpSize> warp = cg::tiled_partition<warpSize>(block);
+    const auto lane_id = warp.thread_rank();
+
+    float val;
+    if(idx < N) {
+        val = input[idx];
+    } else {
+        val = 0.0f;
     }
-    shMem[threadIdx.x] = input[idx];
-
-    int tid = threadIdx.x;
-    int groupDim = 1;
-    __syncthreads();
-    while (true) {
-        const int out_index = groupDim*(tid+1) + threadIdx.x;
-        if(out_index < threadsPerBlock) {
-            shMem[out_index] += shMem[groupDim*(2*tid+1)-1];
-        }
-        tid /= 2;
-        groupDim *= 2;
-        __syncthreads();
-        if(groupDim >= threadsPerBlock) {
-            break;
-        }
+    #pragma unroll
+    for (int bucketDim = 1; 2*bucketDim <= warpSize; bucketDim *= 2) {
+        const int bucketId = lane_id/bucketDim;
+        const int prev_bucket_elem = bucketId*bucketDim-1;
+        const int is_active_bucket = -(bucketId % 2);
+        val += __int_as_float(is_active_bucket & __float_as_int(warp.shfl(val, prev_bucket_elem)));
     }
-    __syncthreads();
 
-    output[idx] = shMem[threadIdx.x];
+    #pragma unroll
+    for (int bucketDim = warpSize; 2*bucketDim <= threadsPerBlock; bucketDim *= 2) {
+        const int bucketId = threadIdx.x/bucketDim;
+        if(threadIdx.x % bucketDim == (bucketDim-1)) { // last thread of each bucket
+            warp_last[bucketId] = val;
+        }
+        block.sync();
+
+        if(bucketId % 2 == 1) {
+            val += warp_last[bucketId - 1];
+        }
+        block.sync();
+    }
+
+    if(idx < N) {
+        output[idx] = val;
+    }
 }
 
 __global__ void merge_prefix_sums_of_blocks(const float* input, float* output, int N, int groupDim) {
